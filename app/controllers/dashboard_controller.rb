356b631aec44b51f7b7ec8db12cc8e99ee3fe264
Matt Townsen <matthewtownsen@gmail.com>
2018-06-10 16:36:25 -0600

    post routes for new answers and more design work

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,4 +2,25 @@ class DashboardController < ApplicationController
   def index
     @partner = current_user.partner
   end
+
+  def newanswer
+    @answer = Answer.new(newanswer_params)
+    @answer.user = current_user
+
+    respond_to do |format|
+      if @answer.save
+        format.html { render action: "index" }
+        format.json { render json: @answer, status: :created, location: @answer }
+      else
+        format.html { render action: "index" }
+        format.json { render json: @answer.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def newanswer_params
+      params.require(:answer).permit(:value, :question_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "dashboard#index"
+  get '/dashboard', to:'dashboard#index', as: :dashboard
+  post '/newanswer', to:'dashboard#newanswer',  as: :newanswer
 end
